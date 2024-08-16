a1321c07f83c1d289d77af71a9dfb7ab9de89fd4
Ishan Audichya <audichyaishan@gmail.com>
2024-08-16 23:50:41 +0530

    frontend finish

---
+++ frontend/src/app/video/[videoId]/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import VideoJS from "@/components/core/videojs";
+import { Button } from "@/components/ui/button";
+import React, { useRef, useState } from "react";
+
+
+const VideoPage = ({ params }: { params: { videoId: string } }) => {
+  const bucketurl = process.env.PUBLIC_BUCKET_URL;
+  const source = `${bucketurl}${params.videoId}`;
+  const playerRef = useRef(null);
+  const [resolution, setResolution] = useState(360);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    plugins: {
+      qualityLevels: {
+        default: "auto",
+      },
+    },
+    sources: [
+      {
+        src: `${source}/output_${resolution}p/hls_${resolution}p.m3u8`,
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+
+  const handlePlayerReady = (player: any) => {
+    playerRef.current = player;
+
+    player.on("waiting", () => {
+      console.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      console.log("player will dispose");
+    });
+  };
+
+  return (
+    <div className="container p-10">
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold">Video</h1>
+        <p className="text-2xl font-bold">{params.videoId}</p>
+      </div>
+
+      <div className="max-w-screen-md mx-auto mt-10 rounded-lg overflow-hidden">
+        <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+      </div>
+
+      <div className="flex gap-3 items-center justify-center mt-5">
+        <Button onClick={() => setResolution(360)}>360p</Button>
+        <Button onClick={() => setResolution(480)}>480p</Button>
+        <Button onClick={() => setResolution(720)}>720p</Button>
+      </div>
+    </div>
+  );
+};
+
+export default VideoPage;
---
+++ frontend/src/components/core/videojs.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import videojs from "video.js";
+import Player from "video.js/dist/types/player";
+import "video.js/dist/video-js.css";
+import "videojs-contrib-quality-levels";
+// import "videojs-hls-quality-selector";
+
+export const VideoJS = (props: any) => {
+  const videoRef = useRef<HTMLDivElement>(null);
+  const playerRef = useRef<Player | null>(null);
+  const { options, onReady } = props;
+
+  useEffect(() => {
+    if (!playerRef.current) {
+      const videoElement = document.createElement("video-js");
+
+      videoElement.classList.add("vjs-big-play-centered");
+      videoRef?.current?.appendChild(videoElement);
+
+      const player = (playerRef.current = videojs(videoElement, options, () => {
+        console.log("player is ready");
+        onReady && onReady(player);
+      }));
+    } else {
+      const player = playerRef.current;
+
+      player.autoplay(options.autoplay);
+      player.src(options.sources);
+      // player?.qualityLevels();
+      // player.hlsQualitySelector({
+      //   displayCurrentQuality: true,
+      // });
+
+      // player.httpSourceSelector();
+    }
+  }, [options, videoRef]);
+
+  useEffect(() => {
+    const player = playerRef.current;
+
+    return () => {
+      if (player && !player.isDisposed()) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
+
+  return (
+    <div data-vjs-player>
+      <div ref={videoRef} />
+    </div>
+  );
+};
+
+export default VideoJS;
